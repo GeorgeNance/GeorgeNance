b58f2578622f00c5e5e1282a7b2996db5673bde8
George Nance <thatbuddhadude@gmail.com>
2020-07-24 14:05:05 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require("isomorphic-unfetch");
+let Parser = require('rss-parser');
 const { promises: fs } = require("fs");
 const path = require("path");
 
@@ -11,10 +12,19 @@ async function main() {
   const { en: qoth, author: qoth_author } = await (
     await fetch("https://programming-quotes-api.herokuapp.com/quotes/random")
   ).json();
+  
+  let feed = await parser.parseURL('https://georgenance.com/rss.xml');
+  
+  let blogPosts = ""
+  
+  feed.items..slice(0, 5)(item => {
+    blogPosts+=`({item.title})[item.link]`
+  });
 
   const readme = readmeTemplate
     .replace("{qoth}", qoth)
     .replace("{qoth_author}", qoth_author)
+    .replace("{blog_posts}", blogPosts)
 
   await fs.writeFile("README.md", readme);
 }
